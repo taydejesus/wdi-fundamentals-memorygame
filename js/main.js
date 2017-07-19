5d1fa893680a9fd93e23699b45892473e787ffe6
DeJesus <pvq576@dca90475ed62.cof.ds.capitalone.com>
2017-07-19 10:27:41 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 console.log("Up and running!");
 
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("flipped " + cardOne);
+console.log("flipped " + cardTwo);
+
+if(cardsInPlay.length === 2) {
+  if(cardOne == cardTwo) {
+    alert("You found a match!");
+  } else {
+    alert("Try again");
+  }
+}
