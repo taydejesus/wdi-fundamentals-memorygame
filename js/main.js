4a9548191142ccea83f02805af2285bd02e1cd23
DeJesus <pvq576@dca90475ed62.cof.ds.capitalone.com>
2017-07-19 12:03:55 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,19 +2,24 @@ console.log("Up and running!");
 
 
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+  {rank: 'queen', suit: 'hearts', cardImage: 'images/queen-of-hearts.png'},
+  {rank: "queen", suit: "diamonds", cardImage: "images/queen-of-diamonds.png"},
+  {rank: "queen", suit: "hearts", cardImage: "images/king-of-hearts.png"},
+  {rank: "queen", suit: "diamonds", cardImage: "images/king-of-diamonds.png"}];
+
 var cardsInPlay = [];
 
-var flipCard = function(cardId) {
 
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 
   if (cardsInPlay.length === 2){
     checkForMatch();
   }
-
-
 }
 
 var checkForMatch = function() {
